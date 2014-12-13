a3029b302b71ff6730a0ffadd151878b18af80f1
vinicius <vinicius.nascimento.silva@usp.br>
2014-12-13 19:40:02 -0300

    Temos auto complete!

--- www/js/pontos/busca-controller.js
+++ www/js/pontos/busca-controller.js
@@ -1,3 +1,20 @@
 "use strict";
-pontosApp.controller('buscaCtrl', [ '$scope', function($scope) {
+pontosApp.controller('buscaCtrl', [ '$scope', '$filter', function($scope, $filter) {
+    
+    $scope.selected = "";
+    $scope.users = [
+      {'id': 1, 'nome': 'Coxinha'}, 
+      {'id': 2, 'nome': 'Arroz'},
+      {'id': 3, 'nome': 'Coxa de Frango'},
+      {'id': 4, 'nome': 'Carne'},
+      {'id': 5, 'nome': 'Batata Frita'},
+    ];
+    
+    $scope.matchBegin = function(expected, actual)
+    {
+	if(actual === null) return;
+	var substr = $filter('limitTo')(expected, actual.length)
+	return $filter('uppercase')(actual) === $filter('uppercase')(substr);
+    };
+    
 } ]);
